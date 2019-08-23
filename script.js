26808882035883de17126eaa00c928462988bbcd
Feraast <feraasaltayeh@gmail.com>
2019-08-23 01:07:06 +0100

    Add files via upload
    Search bar kind of glitches out when you backspace all the way after searching, and I couldn't figure out how to add the functionality which changes the number of pagination links based on number of search results. Other than that, it should meet expectations.

---
+++ script.js
@@ -0,0 +1,131 @@
+let maxItemsPerPage = 10;
+let listItem = document.querySelectorAll('li');
+let docBody = document.body;
+
+
+// This function basically displays 10 items from the list and hides the rest
+function showPage(list,page)
+{
+    
+let startIndex = page * maxItemsPerPage - maxItemsPerPage;
+let endIndex = page * maxItemsPerPage;
+
+    
+for (let i = 0; i < listItem.length; i+=1)
+    {
+        list[i].style.display = 'none';
+    }
+    
+
+for (let i = startIndex; i < endIndex; i+=1)
+    {
+        list[i].style.display = 'block';
+    }
+
+}
+
+function appendPageLinks(list)
+{
+    //This is the section that contains the information about each person
+    let pageDiv = document.querySelector('.page');
+    
+    //Round up since if its like 23 links were gonna need 3 pages
+    let numberOfLinks = Math.ceil(parseInt (listItem.length) / maxItemsPerPage) ;
+    
+    //Create a div tag to clearly indicate the place where the pagination buttons
+    //go
+    let paginationLinksDiv = document.createElement('DIV');
+    paginationLinksDiv.className = 'pagination';
+    
+    //At the end of the pagination links, the div with the button should be put
+    pageDiv.appendChild(paginationLinksDiv);
+
+    //Add the unordered list to the pagination links div
+    let paginationLinksUl = document.createElement('UL');
+    paginationLinksUl.className = 'UlOfButtons';
+    paginationLinksDiv.appendChild(paginationLinksUl);
+    
+    
+    //Go through each link and only activate the li of the page youre on
+    for (let i = 1; i <= numberOfLinks; i+=1)
+    {
+        let paginationLinksLi = document.createElement('LI');
+        let paginationLinksA = document.createElement('A');
+ 
+        let liItem = paginationLinksUl.appendChild(paginationLinksLi);           let listItems = paginationLinksUl.children;
+
+        let liA = liItem.appendChild(paginationLinksA);
+        liA.textContent = i;
+        
+        liA.addEventListener('click', () => { 
+        showPage(listItem,liItem.textContent);
+            
+        for (let i = 0; i < numberOfLinks; i += 1)
+        {
+        listItems[i].className = '';
+        }
+            
+        liItem.className = 'active';
+            
+        } )
+        
+    }
+    
+    //Initialize by setting the first list item to have the class active
+    let listItems = paginationLinksUl.children;
+    listItems[0].className = 'active';
+    
+}
+
+showPage(listItem,1);
+appendPageLinks(docBody);
+
+//Exceeds expectations part
+
+var searchBar = document.createElement("INPUT");
+searchBar.setAttribute("type", "text");
+
+page = document.querySelector('.page');
+page.insertBefore(searchBar,page.firstElementChild);
+
+searchBar.addEventListener('keyup',function(event)
+                           {
+    let counter = 0;
+    let searchTerm = event.target.value.toLowerCase();
+
+    let people = document.querySelectorAll('li');
+    Array.from(people).forEach(function(person){
+  
+    let studentName = person.firstElementChild.textContent;
+        
+    if (studentName.toLowerCase().indexOf(searchTerm) != -1)
+        {
+            person.style.display = 'block';
+            counter+=1;
+        }
+    else 
+        { 
+            person.style.display = 'none';
+            
+        }
+        
+})
+    appendPageLinks(docBody);
+    console.log(counter);
+
+})
+
+//while(searchBar.value === '')
+//    {
+//        
+//     showPage(listItem,1);   
+//        
+//    }
+
+
+
+
+
+
+
+
